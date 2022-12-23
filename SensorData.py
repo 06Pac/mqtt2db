50f3c598d35763a400c5a523306dea1ad7cf3539
oskar <oskartueuer@gmail.com>
2022-12-23 08:19:53 +0200

    parse function modification

--- SensorData.py
+++ SensorData.py
@@ -1,8 +1,11 @@
 import json
+
+
 class SensorData:
     """
         Documentation for a SensorData class.
     """
+
     def __init__(self):
         """
             @brief Class has 6 variables
@@ -23,8 +26,6 @@ class SensorData:
         self.batteey_status = None
         self.latitude = None
         self.longitude = None
-        self.rssi = None
-        self.consumed_airtime = None
 
     def weather_data_parse(self, msg_string):
         """
@@ -32,23 +33,43 @@ class SensorData:
             @param msg_string: takes string of data in json format.
         """
         self.device_id = msg_string['end_device_ids']['device_id']
-        if self.device_id == 'py-saxion' or self.device_id == 'py-wierden':
+        if self.device_id == 'py-saxion':
             self.temperature = msg_string['uplink_message']['decoded_payload']['temperature']
             self.light = msg_string['uplink_message']['decoded_payload']['light']
             self.pressure = msg_string['uplink_message']['decoded_payload']['pressure']
             self.humidity = None
             self.datetime = msg_string['uplink_message']['rx_metadata'][0]['time']
+            self.batteey_status = None
+            self.battery_voltage = None
+            self.latitude = msg_string['uplink_message']['rx_metadata'][0]['location']['latitude']
+            self.longitude = msg_string['uplink_message']['rx_metadata'][0]['location']['longitude']
         elif self.device_id == 'lht-wierden' or self.device_id == 'lht-gronau':
             self.temperature = msg_string['uplink_message']['decoded_payload']['TempC_SHT']
             self.humidity = msg_string['uplink_message']['decoded_payload']['Hum_SHT']
             self.light = None
             self.pressure = None
             self.datetime = msg_string['uplink_message']['rx_metadata'][0]['time']
+            self.batteey_status = msg_string['uplink_message']['decoded_payload']['Bat_status']
+            self.battery_voltage = msg_string['uplink_message']['decoded_payload']['BatV']
+            self.latitude = msg_string['uplink_message']['rx_metadata'][0]['location']['latitude']
+            self.longitude = msg_string['uplink_message']['rx_metadata'][0]['location']['longitude']
         elif self.device_id == 'lht-saxion':
             self.temperature = msg_string['uplink_message']['decoded_payload']['TempC_SHT']
             self.humidity = msg_string['uplink_message']['decoded_payload']['Hum_SHT']
             self.light = None
             self.pressure = None
             self.datetime = msg_string['uplink_message']['rx_metadata'][0]['time']
-
-
+            self.batteey_status = msg_string['uplink_message']['decoded_payload']['Bat_status']
+            self.battery_voltage = msg_string['uplink_message']['decoded_payload']['BatV']
+            self.latitude = msg_string['uplink_message']['rx_metadata'][0]['location']['latitude']
+            self.longitude = msg_string['uplink_message']['rx_metadata'][0]['location']['longitude']
+        elif self.device_id == 'py-wierden':
+            self.temperature = msg_string['uplink_message']['decoded_payload']['temperature']
+            self.light = msg_string['uplink_message']['decoded_payload']['light']
+            self.pressure = msg_string['uplink_message']['decoded_payload']['pressure']
+            self.humidity = None
+            self.datetime = msg_string['uplink_message']['rx_metadata'][0]['time']
+            self.batteey_status = msg_string['uplink_message']['decoded_payload']['Bat_status']
+            self.battery_voltage = msg_string['uplink_message']['decoded_payload']['BatV']
+            self.latitude = msg_string['uplink_message']['rx_metadata'][0]['location']['latitude']
+            self.longitude = msg_string['uplink_message']['rx_metadata'][0]['location']['longitude']
